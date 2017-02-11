5464bc02ad3cb4505355038e980a296e909cb107
Nikolay Genov <nikolay.genov2@gmail.com>
2017-02-12 00:48:41 +0200

    Refactor time extraction to work only for 1 section Comment out some code for later use Create a function for links creation, that are to be crawled for time information Rearrange code

--- crawler/line.go
+++ crawler/line.go
@@ -4,29 +4,31 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"strings"
-
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const (
-	operation_normal_identifier                 = "делник"
-	operation_pre_holiday_identifier            = "предпразник"
-	operation_holiday_identifier                = "празник"
-	operation_pre_holiday_or_holiday_identifier = "предпразник / празник"
-)
-
+/* ========================================================================================= */
 type OperationType int
-
 type OperationTypes []OperationType
+
+//TODO - have to map Transportation Type and Line number to OperationTypeIDMap
+type OperationID string
+type OperationTypeIDMap map[OperationType]OperationID
 
 const (
 	OPERATION_NORMAL OperationType = iota
 	OPERATION_PRE_HOLIDAY
 	OPERATION_HOLIDAY
 	OPERATION_UNKNOWN
+)
+const (
+	operation_normal_identifier                 = "делник"
+	operation_pre_holiday_identifier            = "предпразник"
+	operation_holiday_identifier                = "празник"
+	operation_pre_holiday_or_holiday_identifier = "предпразник / празник"
 )
 
 var operationsIdentifiers = map[string]OperationType{operation_normal_identifier: OPERATION_NORMAL,
@@ -41,67 +43,6 @@ var operationStrings = [...]string{OPERATION_NORMAL: "Weekday",
 func (o OperationType) String() string {
 	return operationStrings[o]
 }
-
-// ===============================================
-//Current date and page postion related functions
-func getLineOperationType(doc *goquery.Document) OperationType {
-	operationTypeRaw := doc.Find(".schedule_active_list_active_tab").Text()
-	switch strings.TrimSpace(operationTypeRaw) {
-	case operation_normal_identifier:
-		return OPERATION_NORMAL
-	case operation_pre_holiday_or_holiday_identifier:
-		return OPERATION_HOLIDAY
-	case operation_holiday_identifier:
-		return OPERATION_HOLIDAY
-	case operation_pre_holiday_identifier:
-		return OPERATION_PRE_HOLIDAY
-	default:
-		panic(fmt.Errorf("No operation mode found %v", operationTypeRaw))
-	}
-}
-
-// =============================================
-type Something struct {
-	Route
-	OperationType
-}
-type Route struct {
-	Direction
-	Stops
-}
-type Routes [2]Route
-
-type Direction struct {
-	Name string
-	ID   string
-}
-type Directions [2]Direction
-
-type Stop struct {
-	Name        string
-	CapitalName string
-	Sign        string
-	ID          string
-	URL         string
-}
-type Stops []Stop
-
-//TODO - have to map Transportation Type and Line number to OperationTypeIDMap
-type OperationID string
-type OperationTypeIDMap map[OperationType]OperationID
-
-const (
-	LINK_SELECTOR                 = "a"
-	LIST_SELECTOR                 = "li"
-	OPERATION_ENTRY_SELECTOR      = ".schedule_active_list_tabs li a"
-	DIRECTION_TAB_SELECTOR        = ".schedule_view_direction_tabs"
-	FULL_ROUTE_SELECTOR           = ".schedule_view_route_directions"
-	STOP_SIGNS_SELECTOR           = ".schedule_direction_signs"
-	STOP_SIGN_SELECTOR            = ".stop_link"
-	STOP_CAPITAL_NAME_SELECTOR    = ".stop_change"
-	SCHEDULE_TIMES_SELECTOR       = ".schedule_times"
-	SCHEDULE_LINKS_TIMES_SELECTOR = "tbody a"
-)
 
 func getOperationsMap(doc *goquery.Document) OperationTypeIDMap {
 	m := make(OperationTypeIDMap)
@@ -132,6 +73,66 @@ func getOperationType(operationTypeString string) OperationType {
 	}
 	return OPERATION_UNKNOWN
 }
+
+/* ========================================================================================= */
+
+//Current date and page postion related functions
+func getLineOperationType(doc *goquery.Document) OperationType {
+	operationTypeRaw := doc.Find(".schedule_active_list_active_tab").Text()
+	switch strings.TrimSpace(operationTypeRaw) {
+	case operation_normal_identifier:
+		return OPERATION_NORMAL
+	case operation_pre_holiday_or_holiday_identifier:
+		return OPERATION_HOLIDAY
+	case operation_holiday_identifier:
+		return OPERATION_HOLIDAY
+	case operation_pre_holiday_identifier:
+		return OPERATION_PRE_HOLIDAY
+	default:
+		panic(fmt.Errorf("No operation mode found %v", operationTypeRaw))
+	}
+}
+
+/* ========================================================================================= */
+type LineSomething struct {
+	Routes
+	OperationTypeIDMap
+}
+
+type Route struct {
+	Direction
+	Stops
+}
+type Routes [2]Route
+
+type Direction struct {
+	Name string
+	ID   string
+}
+type Directions [2]Direction
+
+type Stop struct {
+	Name        string
+	CapitalName string
+	Sign        string
+	ID          string
+	URL         string
+}
+type Stops []Stop
+
+/* ========================================================================================= */
+
+const (
+	LINK_SELECTOR                 = "a"
+	LIST_SELECTOR                 = "li"
+	OPERATION_ENTRY_SELECTOR      = ".schedule_active_list_tabs li a"
+	DIRECTION_TAB_SELECTOR        = ".schedule_view_direction_tabs"
+	FULL_ROUTE_SELECTOR           = ".schedule_view_route_directions"
+	STOP_SIGNS_SELECTOR           = ".schedule_direction_signs"
+	STOP_SIGN_SELECTOR            = ".stop_link"
+	STOP_CAPITAL_NAME_SELECTOR    = ".stop_change"
+	SCHEDULE_LINKS_TIMES_SELECTOR = ".schedule_times tbody a"
+)
 
 func getRoutes(doc *goquery.Document) Routes {
 	//We only select one of both because the others results from their selection contains the same results
@@ -203,107 +204,116 @@ func getStopURLAndID(url string) (string, string) {
 	return url, id
 }
 
-func getNormalTimes(doc *goquery.Document) [][]string {
-	timesOfTimes := make([][]string, 0)
-	doc.Find(SCHEDULE_TIMES_SELECTOR).Each(func(i1 int, st *goquery.Selection) {
-		times := make([]string, 0)
-		st.Find(SCHEDULE_LINKS_TIMES_SELECTOR).
-			Each(func(i int, s *goquery.Selection) {
-				times = append(times, strings.TrimSpace(s.Text()))
-			})
-		timesOfTimes = append(timesOfTimes, times)
-	})
-	return timesOfTimes
-}
+/* ========================================================================================= */
 
-func advancedTimes(doc *goquery.Document) [][][]string {
-
-	timesOfTimes := make([][][]string, 0)
-	doc.Find(SCHEDULE_TIMES_SELECTOR).Each(func(i1 int, st *goquery.Selection) {
-		times := make([][]string, 0)
-		st.Find(SCHEDULE_LINKS_TIMES_SELECTOR).Each(func(i int, s *goquery.Selection) {
-			click, _ := s.Attr("onclick")
-			//fmt.Println(click)
-			i2 := strings.LastIndex(click, "'")
-			i1 := strings.LastIndex(click[:i2], "'")
-			reduced := click[i1+1 : i2]
-			splits := strings.Split(reduced, ",")
-			times = append(times, splits)
+func getNormalTimes(doc *goquery.Document) []string {
+	times := make([]string, 0)
+	doc.Find(SCHEDULE_LINKS_TIMES_SELECTOR).
+		Each(func(i int, s *goquery.Selection) {
+			times = append(times, strings.TrimSpace(s.Text()))
 		})
-		timesOfTimes = append(timesOfTimes, times)
-	})
-	return timesOfTimes
-
+	return times
 }
 
-func intToTime(c string) string {
-	i, _ := strconv.Atoi(c)
-	return fmt.Sprintf("%v:%02d", i/60, i%60)
-}
+//func getNormalTimesOfTimes(doc *goquery.Document) [][]string {
+//	timesOfTimes := make([][]string, 0)
+//	doc.Find(SCHEDULE_TIMES_SELECTOR).Each(func(i1 int, st *goquery.Selection) {
+//		times := make([]string, 0)
+//		st.Find(SCHEDULE_LINKS_TIMES_SELECTOR).
+//			Each(func(i int, s *goquery.Selection) {
+//				times = append(times, strings.TrimSpace(s.Text()))
+//			})
+//		timesOfTimes = append(timesOfTimes, times)
+//	})
+//	return timesOfTimes
+//}
 
-func printTimes(times [][]string) {
-	l := len(times[0])
-	for i := 1; i < l; i++ {
-		for _, row := range times {
+//func advancedTimes(doc *goquery.Document) [][][]string {
+//	timesOfTimes := make([][][]string, 0)
+//	doc.Find(SCHEDULE_TIMES_SELECTOR).Each(func(i1 int, st *goquery.Selection) {
+//		times := make([][]string, 0)
+//		st.Find(SCHEDULE_LINKS_TIMES_SELECTOR).Each(func(i int, s *goquery.Selection) {
+//			click, _ := s.Attr("onclick")
+//			//fmt.Println(click)
+//			i2 := strings.LastIndex(click, "'")
+//			i1 := strings.LastIndex(click[:i2], "'")
+//			reduced := click[i1+1 : i2]
+//			splits := strings.Split(reduced, ",")
+//			times = append(times, splits)
+//		})
+//		timesOfTimes = append(timesOfTimes, times)
+//	})
+//	return timesOfTimes
+//}
+//func intToTime(c string) string {
+//	i, _ := strconv.Atoi(c)
+//	return fmt.Sprintf("%v:%02d", i/60, i%60)
+//}
+//
+//func printTimes(times [][]string) {
+//	l := len(times[0])
+//	for i := 1; i < l; i++ {
+//		for _, row := range times {
+//
+//			time := row[i]
+//
+//			if time != "" {
+//				fmt.Printf("%v\t", intToTime(time))
+//			} else {
+//				fmt.Print("*****\t")
+//			}
+//		}
+//		fmt.Print("\n")
+//	}
+//}
 
-			time := row[i]
-
-			if time != "" {
-				fmt.Printf("%v\t", intToTime(time))
-			} else {
-				fmt.Print("*****\t")
-			}
-		}
-		fmt.Print("\n")
-	}
-}
-
-func buildLinks(doc *goquery.Document) []string {
-	m := getOperationsMap(doc)
-	routes := getRoutes(doc)
+/* ========================================================================================= */
+func buildLinkSuffixes(baseURL string, something LineSomething) []string {
 	links := make([]string, 0)
-	for _, v := range m {
-		operationID := v
-		for _, directionStop := range routes {
-			directionID := directionStop.ID
-			for _, stop := range directionStop.Stops {
+	for _, operationID := range something.OperationTypeIDMap {
+		for _, route := range something.Routes {
+			for _, stop := range route.Stops {
 				stopID, _ := strconv.Atoi(stop.Sign)
 				links = append(links,
-					fmt.Sprintf("%v/%v/%v", operationID, directionID, stopID))
+					fmt.Sprintf("%v/%v/%v/%v", baseURL, operationID, route.Direction.ID, stopID))
 			}
-
 		}
 	}
 	return links
 }
+func makeLineSomething(doc *goquery.Document) LineSomething {
+	return LineSomething{getRoutes(doc), getOperationsMap(doc)}
+}
+
+func createLinksForCrawling(baseURL string, doc *goquery.Document) []string {
+	lineSomething := makeLineSomething(doc)
+	links := buildLinkSuffixes(baseURL, lineSomething)
+	return links
+}
+
+/* ========================================================================================= */
 
 func CrawlLine(line LineNameAndURL, r io.Reader) {
-	//doc, err := goquery.NewDocument("http://schedules.sofiatraffic.bg/server/html/schedule_load/6672/2696/377")
-	//fmt.Println(doc.Text())
 	doc, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	links := buildLinks(doc)
-
-	//URL := "http://schedules.sofiatraffic.bg/server/html/schedule_load/"
-	//d, _ := goquery.NewDocument(URL + links[0])
+	//links := buildLinkSuffixes(doc)
+	//fmt.Println(links)
+	URL := "http://schedules.sofiatraffic.bg/server/html/schedule_load"
+	links := createLinksForCrawling(URL, doc)
+	//d, _ := goquery.NewDocument(links[0])
 	//fmt.Println(d.Text())
 	//for _, link := range links {
-	//	d, _ := goquery.NewDocument(URL + link)
+	//	d, _ := goquery.NewDocument(link)
 	//	times := getNormalTimes(d)
 	//	fmt.Printf("%v -> %v\n", link, times)
 	//}
 
-	times := getNormalTimes(doc)
-	for _, time := range times {
-		fmt.Println(time)
-	}
-	advancedTimes := advancedTimes(doc)
-	for _, time := range advancedTimes {
-		fmt.Println(time)
-	}
+	//times := getNormalTimes(d)
+	//fmt.Println(times)
+
 	fmt.Println(links)
 	fmt.Println(len(links))
 }
